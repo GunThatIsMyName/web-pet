ad5d2ddd5347a5d4b12f1b30aa7165c808d700c2
Junhyun <mnmthatisus@gmail.com>
2022-01-07 10:28:56 +0900

    pause here

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Navbar} from './components';
-import Control from './components/Control';
-import Loader from './components/Loader';
-import Login from './components/Login';
-import {useUserContext} from './context/UserContext';
-import {Game, Home, Show} from './routes';
-import Shop from './routes/Shop';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
+import Control from "./components/Control";
+import Loader from "./components/Loader";
+import Login from "./components/Login";
+import { useUserContext } from "./context/UserContext";
+import { Game, Home, Show } from "./routes";
+import Shop from "./routes/Shop";
 
 const App = () => {
-  const {user, loading} = useUserContext();
-  const {name, photo, email} = user;
-  const isLoggedIn = name !== '' && photo !== '' && email !== '';
+  const { user, loading } = useUserContext();
+  const { name, photo, email } = user;
+  const isLoggedIn = name !== "" && photo !== "" && email !== "";
 
   if (loading) {
     return <Loader />;
@@ -22,11 +22,17 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/game" element={isLoggedIn ? <Game /> : <Login />}>
-            <Route path="" element={isLoggedIn ? <Control /> : <Login />} />
-            <Route path="shop" element={isLoggedIn ? <Shop /> : <Login />} />
-          </Route>
-          <Route path="/show" element={isLoggedIn ? <Show /> : <Login />} />
+          {isLoggedIn ? (
+            <>
+              <Route path="/game" element={<Game />}>
+                <Route path="" element={<Control />} />
+                <Route path="shop" element={<Shop />} />
+              </Route>
+              <Route path="/show" element={<Show />} />
+            </>
+          ) : (
+            <Login />
+          )}
         </Routes>
       </BrowserRouter>
     </div>
